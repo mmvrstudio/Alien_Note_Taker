1b8306b604c80d1238deb149602fe5c5f767ff0d
Mariana Marques <65464431+mmvrstudio@users.noreply.github.com>
2022-03-29 12:53:34 -0400

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,6 @@
-// Dependencies
 const path = require("path");
 
-// Routing
-
 module.exports = function (app){
-
-    // HTML GET Requests
-    // Below code handles when users 'visit' a page.
-    // In each of the below cases the user is shown an HTML page of content
-
-    
     app.get('/notes', function(request, response) {
         response.sendFile(path.join(__dirname, '../public/notes.html'));
     });
