d95d254b9d4d6bbc228eb3dd00641c7285eca534
Mariana Marques <65464431+mmvrstudio@users.noreply.github.com>
2022-03-29 10:55:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-// Dependencies
+
 const express = require("express");
 
-// Express configuration
-//Tells node that we are creating an 'express' server
+
 const app = express();
 
-// Sets an initial port.
+
 const PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
