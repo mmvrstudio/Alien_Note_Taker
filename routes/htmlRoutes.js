2cd5ede95eddfa5b750090c08672626ca09c56f9
Mariana Marques <65464431+mmvrstudio@users.noreply.github.com>
2022-04-12 08:52:26 -0400

    Delete routes directory

--- routes/htmlRoutes.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-module.exports = function (app){
-    app.get('/notes', function(request, response) {
-        response.sendFile(path.join(__dirname, '../public/notes.html'));
-    });
-    app.get('*', function(request, response) {
-        response.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-};
